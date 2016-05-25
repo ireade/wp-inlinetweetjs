40686e0829301dc6c6e51d05effb6cad9da2f7e9
Daniel Bishop <bishless@gmail.com>
2016-05-25 10:53:28 -0700

    bumping version

--- inlineTweets.php
+++ inlineTweets.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Plugin Name: InlineTweets
 Description: InlineTweets allows you to easily create tweetable links out of any text on a webpage.
@@ -55,7 +55,7 @@ if( !function_exists("inline_tweets_page") ) {
 				<th scope="row">Inline Tweets Wrapper (default is &lt;span&gt;)</th>
 				<td><input type="text" name="inline_tweets_wrapper" value="<?php echo esc_attr( get_option('inline_tweets_wrapper') ); ?>"/></td>
 			</tr>
-		</table>		
+		</table>
 		<?php submit_button(); ?>
 	</form>
 	<h2>How to Use</h2>
@@ -77,23 +77,23 @@ if( !function_exists("inline_tweets_page") ) {
 /* Create shortcode */
 if (!function_exists("inline_tweet_link")) {
 	add_shortcode("inlinetweet", "inline_tweet_link");
-	
+
 	function inline_tweet_link( $atts, $content = null ) {
-		
+
 		extract(shortcode_atts(array(
 			"via" => "",
 			"tags" => "",
 			"url" => "",
 			"wrapper" => ""
 		), $atts));
-		
+
 		wp_enqueue_style( 'inlineTweets', plugins_url('assets/inline-tweet.min.css',__FILE__ ));
 		wp_enqueue_script( 'inlineTweets', plugins_url('assets/inline-tweet.min.js',__FILE__ ),array(jquery));
-		
+
 		global $wp;
 		//$current_url = home_url(add_query_arg(array(),$wp->request));
 		$current_url = set_url_scheme( 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
-		
+
 		// check for local setting first
 		if (isset($atts['via'])) {
 			$via = $atts['via'];
@@ -104,7 +104,7 @@ if (!function_exists("inline_tweet_link")) {
 		} else {
 			$via = "";
 		}
-		
+
 		// check for local setting first
 		if (isset($atts['tags'])) {
 			$tags = $atts['tags'];
@@ -115,7 +115,7 @@ if (!function_exists("inline_tweet_link")) {
 		} else {
 			$tags = "";
 		}
-		
+
 		// check for local setting first
 		if (isset($atts['url'])) {
 			$url = $atts['url'];
@@ -123,7 +123,7 @@ if (!function_exists("inline_tweet_link")) {
 			// if no user setting, use current page URL
 			$url = $current_url;
 		}
-		
+
 		// check for local setting first
 		if (isset($atts['wrapper'])) {
 			$wrapper = $atts['wrapper'];
@@ -134,7 +134,7 @@ if (!function_exists("inline_tweet_link")) {
 		} else {
 			$wrapper = "span";
 		}
-		
+
 		if ( $via != "") {
 			$setVia = ' data-inline-tweet-via="'. $via .'"';
 		}
@@ -146,6 +146,66 @@ if (!function_exists("inline_tweet_link")) {
 		}
 
 		return '<'.$wrapper.' data-inline-tweet'.$setVia.$setTags.$setUrl.'>'.$content.'</'.$wrapper.'>';
-	}	
+	}
 }
+
+// if ( function_exists( 'register_shortcode_ui' ) ) {
+	// add_action( 'register_shortcode_ui', 'shortcode_ui_inlinetweets' );
+	// /**
+	//  * Register a UI for the inlinetweet shortcode
+	//  */
+	// function shortcode_ui_inlinetweets() {
+	// 	$inlinetweet_fields = array(
+	// 		array(
+	// 			'label' => esc_html__( 'via', 'rttheme11up' ),
+	// 			'attr' => 'via',
+	// 			'type' => 'text',
+	// 			'placeholder' => esc_html__( 'if empty, global setting', 'rttheme11up' ),
+	// 		),
+	// 		array(
+	// 			'label' => 'tags',
+	// 			'attr' => 'tags',
+	// 			'type' => 'text',
+	// 		),
+	// 		array(
+	// 			'label' => esc_html__( 'URL', 'rttheme11up' ),
+	// 			'attr' => 'url',
+	// 			'type' => 'url',
+	// 		),
+	// 		array(
+	// 			'label' => esc_html__( 'Wrapper', 'rttheme11up' ),
+	// 			'attr' => 'wrapper',
+	// 			'type' => 'text',
+	// 			'placeholder' => esc_html__( 'if empty, span', 'rttheme11up' ),
+	// 		),
+	// 	);
+	// 	$inlinetweet_args = array(
+	// 		/*
+	// 		 * How the shortcode should be labeled in the UI. Required argument.
+	// 		 */
+	// 		'label' => esc_html__( 'Inline Tweet', 'rttheme11up' ),
+	// 		/*
+	// 		 * Include an icon with your shortcode. Optional.
+	// 		 * Use a dashicon, or full URL to image.
+	// 		 */
+	// 		'listItemImage' => 'dashicons-twitter',
+	// 		/*
+	// 		 * Register UI for the "inner content" of the shortcode. Optional.
+	// 		 * If no UI is registered for the inner content, then any inner content
+	// 		 * data present will be backed-up during editing.
+	// 		 */
+	// 		'inner_content' => array(
+	// 			'label'        => esc_html__( 'Tweetable text', 'rttheme11up' ),
+	// 			'description'  => esc_html__( 'Keep it brief!', 'rttheme11up' ),
+	// 		),
+	// 		/*
+	// 		 * Define the UI for attributes of the shortcode. Optional.
+	// 		 *
+	// 		 * See above, to where the the assignment to the $fields variable was made.
+	// 		 */
+	// 		'attrs' => $inlinetweet_fields,
+	// 	);
+	// 	shortcode_ui_register_for_shortcode( 'inlinetweet', $inlinetweet_args);
+	// }
+// }
 ?>
